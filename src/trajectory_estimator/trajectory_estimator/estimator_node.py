bc298f22b6407b23323db85f751f843eb7e98dee
DexterFixxor <48955668+DexterFixxor@users.noreply.github.com>
2025-09-23 14:24:46 +0200

    initial xarm setup with ball tracking

--- src/trajectory_estimator/trajectory_estimator/estimator_node.py
+++ src/trajectory_estimator/trajectory_estimator/estimator_node.py
@@ -1,38 +1,38 @@
 import rclpy
-import rclpy.logging
 from rclpy.node import Node
-# from geometry_msgs.msg import Point
 from vicon_receiver.msg import Position
-
-from abb_interface.srv import EEPose
-from .estimator import Estimator
-
-
 import numpy as np
-from typing import Union
 
-from utils import construct_new_frame
+from .estimator import Estimator
+from .utils import construct_new_frame
+from xarm.wrapper import XArmAPI
 
 class TrajectoryEstimator(Node):
   
   def __init__(self):
     super().__init__("trajectory_estimator")
-    # MoveL client
-    self.move_l_client = self.create_client(EEPose, "move_linear")
-    self.req = EEPose.Request()
+    
+    
+    # xArm robot init
+    self.ip = '10.1.108.144'
+    self.arm = XArmAPI(self.ip)
+    self.arm.motion_enable(enable=True)
+    self.arm.set_mode(1)
+    self.arm.set_state(state=0)
+    self.arm_speed = 50
     
     # Vicon subscriber
     self.vicon_sub = self.create_subscription(Position, 
-                                              "vicon/ball/seg", 
+                                              "vicon/PurpleBall_10Markers/ball", 
                                               self.vicon_callback,
                                               qos_profile = 10)
     
     
-    p_origin = np.array([])
-    p_x_dir = np.array([])
-    p_y_dir = np.array([])
+    p_origin = np.array([301.4, -67.6, -124.6])
+    p_x_dir = np.array([461.6, -68.6, -121.6])
+    p_y_dir = np.array([305.2, 180.0, -123.2])
     
-
+    self.T_base_vicon = construct_new_frame(p_origin, p_x_dir, p_y_dir)
     
     self.xyz_min = [20.0, 100.0, 150]
     self.xyz_max = [220, 500, 300]
@@ -48,58 +48,36 @@ class TrajectoryEstimator(Node):
           keep_last_gauss = 100,
           flg_use_nn = True
       )
-    
+    self.counter = 0
     print("\n################### Estimator CONFIG ###################\n")
     print(self.estimator)
     print("\n########################################################\n")
     
     # ---------------------------------------
     print("\nStarting Trajectory Estimator node...\n\n")
-
-  def send_request(self, coords : np.ndarray):
-      
-      self.req.x = float(coords[0])
-      self.req.y = float(coords[1])
-      self.req.z = float(coords[2])
-      
-      self.req.qw = 0.707106781
-      self.req.qx = 0.0
-      self.req.qy = 0.707106781
-      self.req.qz = 0.0
-      
-      # self.req.qw = 0.5
-      # self.req.qx = 0.0
-      # self.req.qy = 0.866025404
-      # self.req.qz = 0.0
-      
-      #self.req.lin_vel = 1000.
-      #self.req.ang_vel = 200.      
-      # self.get_logger().info(f"Sending request: {coords}")
-      
-      future_ret = self.move_l_client.call_async(self.req)
-      future_ret.add_done_callback(self.service_future_callback)
-      
-  
-  def service_future_callback(self, future):
-      # self.get_logger().info("Service done")
-      pass
     
   def vicon_callback(self, msg : Position):
       np_msg = np.array([msg.x_trans, msg.y_trans, msg.z_trans], dtype=np.float32)
-      prev_has_converged = self.estimator.has_converged()
+
       try:
         self.estimator.position_callback(np_msg)
         
       except RuntimeError as e:
         print("Error in estimation: ", e)    
         
-      # if DKL converged send request to MoveL service
-      if self.estimator.has_converged():
-        self.send_request(self.estimator.saved_coord)
-        self.estimator.gauss.has_converged = False
-        if not prev_has_converged:
-          self.estimator.reset()
+      self.counter += 1
+      
+      # if msg.z_trans > 150:
+      #   target_pose = self.T_base_vicon @ np.array([0.0, 0.0, 1.0, 1], dtype=np.float32)
+      #   self.arm.set_position(*target_pose[:3].tolist(), speed = 50)
+      # if (- 100 < msg.x_trans < 280) and \
+      #    (- 150 < msg.y_trans < 260) and \
+      #    ( 150 < msg.z_trans < 400):# and (self.counter % 25 == 0):
+           
+      #   target_pose = self.T_base_vicon @ np.array([msg.x_trans, msg.y_trans, msg.z_trans, 1], dtype=np.float32)
         
+      #   mvpose = [target_pose[0], target_pose[1], target_pose[2] + 40, 180, 0, 0]
+      #   self.arm.set_servo_cartesian(mvpose, speed = 50, mvacc = 1000)
         
       
 def main(args = None):
